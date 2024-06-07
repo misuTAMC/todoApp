7a9eb3687fd11b7c630b32e59bda4423121c4986
2Anh <62981228+misuTAMC@users.noreply.github.com>
2024-06-07 22:48:11 +0700

    submit login_screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:awesome_notifications/awesome_notifications.dart';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,16 +1,9 @@
 
 // ignore_for_file: avoid_print, no_leading_underscores_for_local_identifiers
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
 
-pickImage(ImageSource source) async {
-  final ImagePicker _imagePicker = ImagePicker();
-  XFile? _file = await _imagePicker.pickImage(source: source);
-  if (_file != null) {
-    return await _file.readAsBytes();
-  }
-  print('No image selected.');
-}
+
+
 
 showSnackBar(BuildContext context, String message) {
   ScaffoldMessenger.of(context).showSnackBar(
